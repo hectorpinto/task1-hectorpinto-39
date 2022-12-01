7017caa419a2dd04eaea772f581f711eece21830
hectorpinto <107717073+hectorpinto@users.noreply.github.com>
2022-12-01 10:18:54 -0300

    hector pinto task1

---
+++ java.js
@@ -0,0 +1,12 @@
+
+
+let miNombre = "Hector"
+let miApellido = "Pinto"
+let miEdad = 26
+let miMascota = "Luna"
+let edadMascota = 13
+let nombreCompleto = miNombre +" "+ miApellido 
+
+console.log (nombreCompleto)
+
+
